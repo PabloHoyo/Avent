36e7789249d81d4da1aa536e66de9cccc50a6287
PabloHoyo <pablo.h@wearephenix.com>
2023-12-01 13:16:15 +0100

    Trebuchet

---
+++ 1/Trebuchet.cs
@@ -0,0 +1,17 @@
+﻿namespace Avent;
+
+internal class Trebuchet
+{
+    public int Calculate()
+    {
+        int result = 0;
+        var lines = File.ReadAllLines("./1/input.txt");
+        foreach (var line in lines) 
+        {
+            var first = line.First(char.IsDigit);
+            var last = line.Last(char.IsDigit);
+            result += Convert.ToInt32(string.Concat(first, last));
+        }
+        return result;
+    }
+}
