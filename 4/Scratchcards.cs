bb3e8fe8d7b37b648db5cda63979a07a26826283
PabloHoyo <pablo.h@wearephenix.com>
2023-12-04 10:37:28 +0100

    Day 4

---
+++ 4/Scratchcards.cs
@@ -0,0 +1,54 @@
+﻿namespace Avent;
+
+internal class Scratchcards : Puzzle, IPuzzle
+{
+    public Scratchcards() : base("./4/input.txt") { }
+
+    public int Part1()
+    {
+        var result = 0;
+        foreach (var line in lines)
+        {
+            var winningNumbers = line.Split(':')[1].Split('|')[0].Trim().Split(' ').Where(s => s != string.Empty);
+            var playingNumbers = line.Split(':')[1].Split('|')[1].Trim().Split(' ').Where(s => s != string.Empty);
+            var matches = winningNumbers.Intersect(playingNumbers);
+            result += matches.Any() ? (int)Math.Pow(2, matches.Count() - 1) : 0;
+        }
+        return result;
+    }
+
+    public int Part2()
+    {
+        var result = 0;
+        var matchCounters = new Dictionary<int, int>(); // [cardId, matchesCount]
+        var copiesToProcess = new Dictionary<int, int>(); // [cardId, numberOfCopies]
+        foreach (var line in lines)
+        {
+            var cardId = int.Parse(line.Split(':')[0].Split(' ').Where(s => s != string.Empty).Last());
+            var winningNumbers = line.Split(':')[1].Split('|')[0].Trim().Split(' ').Where(s => s != string.Empty);
+            var playingNumbers = line.Split(':')[1].Split('|')[1].Trim().Split(' ').Where(s => s != string.Empty);
+            matchCounters[cardId] = winningNumbers.Intersect(playingNumbers).Count();
+            copiesToProcess[cardId] = 1;
+            result++;
+        }
+
+        foreach (var cardId in copiesToProcess.Keys) 
+        { 
+            ProcessCard(cardId);
+        }
+
+        return result;
+
+        void ProcessCard(int cardId)
+        {
+            if (!matchCounters.ContainsKey(cardId)) return;
+
+            var matches = matchCounters[cardId];
+            for (int i = 1; i <= matches; i++)
+            {
+                result ++;
+                ProcessCard(cardId + i);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿using Avent;
 
-Console.WriteLine(new Trebuchet().Part1());
-Console.WriteLine(new Trebuchet().Part2());
+//Console.WriteLine(new Trebuchet().Part1());
+//Console.WriteLine(new Trebuchet().Part2());
 
-Console.WriteLine(new CubeConundrum().Part1());
-Console.WriteLine(new CubeConundrum().Part2());
+//Console.WriteLine(new CubeConundrum().Part1());
+//Console.WriteLine(new CubeConundrum().Part2());
 
-Console.WriteLine(new GearRatios().Part1());
-Console.WriteLine(new GearRatios().Part2());
+//Console.WriteLine(new GearRatios().Part1());
+//Console.WriteLine(new GearRatios().Part2());
+
+//Console.WriteLine(new Scratchcards().Part1());
+Console.WriteLine(new Scratchcards().Part2());
 
